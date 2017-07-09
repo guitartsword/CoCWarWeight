c3349871d4be5d5c17fa2790381aa527a19e8aab
Isaias Valle <guitartsword@gmail.com>
2017-07-09 14:46:17 -0600

    added react-router

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-"""falsk starter."""
-from flask import Flask, render_template
+"""flask starter."""
+from flask import Flask, render_template, send_from_directory
 WSGI_APLLICATION = Flask(__name__)
 WSGI_APLLICATION.config['TEMPLATES_AUTO_RELOAD'] = True
+@WSGI_APLLICATION.route('/<path>')
 @WSGI_APLLICATION.route('/')
-def hello_world():
+def hello_world(path=''):
     """renders template."""
     return render_template('index.html')
-
 if __name__ == '__main__':
-    WSGI_APLLICATION.run(debug=True, port=8080)
+    WSGI_APLLICATION.run(host='0.0.0.0', debug=True, port=8080)
--- js/Hello.js
+++ js/Hello.js
@@ -1,14 +1,42 @@
-import React from 'react';
-
-var Hello = React.createClass({
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+class Hello extends Component{
   render() {
-    return (
+    const Home = () => (
       <div>
-        <h1>Hello, world</h1>
+        <h2>Home</h2>
       </div>
+    )
+    const About = () => (
+      <div>
+        <h2>About</h2>
+      </div>
+    )
+    const Topics = () => (
+      <div>
+        <h1>Topics</h1>
+      </div>
+    )
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/topics" component={Topics}></Route>
+        </div>
+      </Router>
     );
   }
-});
+}
 
 export default Hello;
 
